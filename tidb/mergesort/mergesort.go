2f8fa1df8f798a2e40f3d4503305d9785a8f2462
Ziyi Yan <ziyi.yan23@gmail.com>
2019-04-16 17:51:24 +0800

    finish first working version which outperforms sort.Slice()

--- tidb/mergesort/mergesort.go
+++ tidb/mergesort/mergesort.go
@@ -1,6 +1,11 @@
 package main
 
+import (
+	"pingcap/talentplan/tidb/mergesort/kway"
+)
+
 // MergeSort performs the merge sort algorithm.
 // Please supplement this function to accomplish the home work.
 func MergeSort(src []int64) {
+	kway.Sort(src)
 }
