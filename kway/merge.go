52c9e4622f64cc08abafbce3224239ba750e2129
Ziyi Yan <ziyi.yan23@gmail.com>
2019-04-16 15:03:54 +0800

    implement merge and passed some hand-written tests

--- kway/merge.go
+++ kway/merge.go
@@ -1,6 +1,7 @@
 package kway
 
 import (
+	"math"
 	"runtime"
 	"sort"
 	"sync"
@@ -46,7 +47,132 @@ func split(data []int64, n int) (segs [][]int64) {
 // Merge performs a K-way Merge using Tournament Tree
 // algorithm (https://en.wikipedia.org/wiki/K-way_merge_algorithm#Tournament_Tree).
 func Merge(arrs ...[]int64) []int64 {
-	panic("implement me")
+	// resultArr, resultIdx
+	total := 0
+	for _, a := range arrs {
+		total += len(a)
+	}
+	resultArr := make([]int64, total)
+	resultIdx := 0
+
+	// arrs, idxArr, tournamentTree
+	n := len(arrs)
+	idxArr := make([]int, n)
+	tournamentTree := NewTournament(arrs)
+
+	// run tournament
+	for {
+		winner, winnerIdx := tournamentTree.Winner()
+
+		resultArr[resultIdx] = winner
+		resultIdx++
+		if resultIdx == total {
+			break
+		}
+
+		if nextElement, arr := idxArr[winnerIdx]+1, arrs[winnerIdx]; nextElement < len(arr) {
+			tournamentTree.Replace(winnerIdx, arr[nextElement])
+			idxArr[winnerIdx] = nextElement
+		} else {
+			tournamentTree.Replace(winnerIdx, math.MaxInt64)
+		}
+	}
+
+	return resultArr
+}
+
+type Tournament struct {
+	tree []Node
+	size int
+	arrs [][]int64
+}
+
+type Node struct {
+	element  int64
+	ArrIndex int
+}
+
+func NewTournament(arrs [][]int64) *Tournament {
+	n := len(arrs)
+	size := 1
+	nextLine := 2
+	for ; nextLine < n; nextLine *= 2 {
+		size += nextLine
+	}
+	remain := n - nextLine/2
+	size += 2 * remain
+
+	tm := &Tournament{
+		tree: make([]Node, size+1),
+		size: size,
+		arrs: arrs,
+	}
+	tm.initialBattle(arrs)
+	return tm
+}
+
+func (tm *Tournament) Replace(arrIndex int, element int64) {
+	tm.setElement(arrIndex, element)
+	tm.promote(tm.toTreeIndex(arrIndex), Node{element, arrIndex})
+}
+
+func (tm *Tournament) Winner() (int64, int) {
+	winner := tm.tree[0]
+	return winner.element, winner.ArrIndex
+}
+
+func (tm *Tournament) setElement(arrIndex int, element int64) {
+	tm.tree[tm.toTreeIndex(arrIndex)]= Node{element, arrIndex}
+}
+
+func (tm *Tournament) toTreeIndex(idx int) int {
+	return tm.size - idx
+}
+
+func (tm *Tournament) initialBattle(arrs [][]int64) {
+	for i := 0; i < len(arrs); i++ {
+		tm.setElement(i, arrs[i][0])
+	}
+	root := 1
+	winner := tm.build(root)
+	tm.tree[0] = winner
+}
+
+func (tm *Tournament) build(index int) Node {
+	if tm.isLeaf(index) {
+		return tm.tree[index]
+	}
+	leftWinner := tm.build(index * 2)
+	rightWinner := tm.build(index*2 + 1)
+	winner, loser := play(leftWinner, rightWinner)
+	tm.tree[index] = loser
+	return winner
+}
+
+func (tm *Tournament) isLeaf(index int) bool {
+	return len(tm.tree)-len(tm.arrs) <= index && index < len(tm.tree)
+}
+
+func (tm *Tournament) promote(index int, node Node) {
+	curr := index / 2
+	promotingNode := node
+	for curr > 0 {
+		if promotingNode.element > tm.tree[curr].element {
+			loser := promotingNode
+			promotingNode = tm.tree[curr]
+			tm.tree[curr] = loser
+		}
+		curr = curr / 2
+	}
+	tm.tree[0] = promotingNode
+}
+
+func play(leftWinner Node, rightWinner Node) (Node, Node) {
+	if leftWinner.element < rightWinner.element {
+		return leftWinner, rightWinner
+	} else {
+		return rightWinner, leftWinner
+	}
 }
 
 // Int64Slice attaches the methods of Interface to []int64, sorting in increasing order.
--- kway/merge_test.go
+++ kway/merge_test.go
@@ -1,7 +1,75 @@
 package kway
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestMerge(t *testing.T) {
+	type args struct {
+		arrs [][]int64
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int64
+	}{
+		{
+			"simple",
+			args{
+				[][]int64{
+					{1, 3, 5, 7},
+					{2, 4, 6, 8},
+				},
+			},
+			[]int64{1, 2, 3, 4, 5, 6, 7, 8},
+		}, {
+			"four arrays",
+			args{
+				[][]int64{
+					{1, 5, 9, 13},
+					{2, 6, 10, 14},
+					{3, 7, 11, 15},
+					{4, 8, 12, 16},
+				},
+			},
+			[]int64{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16},
+		}, {
+			"variant-length arrays",
+			args{
+				[][]int64{
+					{1, 5, 9, 13, 17, 18},
+					{2, 6, 10, 14, 19, 20},
+					{3, 7, 11, 15},
+					{4, 8, 12, 16},
+					{21, 22, 23},
+				},
+			},
+			[]int64{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Merge(tt.args.arrs...); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Merge() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
 
+func TestSort(t *testing.T) {
+	type args struct {
+		data []int64
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			Sort(tt.args.data)
+		})
+	}
 }
